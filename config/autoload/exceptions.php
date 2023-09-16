5af91f6d09235b2d24bd16a40d1744a79222957a
Gustavo Alexandre <gusttavo212@gmail.com>
2023-09-16 14:34:16 -0300

    Add exception handler

--- config/autoload/exceptions.php
+++ config/autoload/exceptions.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 return [
     'handler' => [
         'http' => [
+            \Hyperf\Validation\ValidationExceptionHandler::class,
             Hyperf\HttpServer\Exception\Handler\HttpExceptionHandler::class,
             App\Exception\Handler\AppExceptionHandler::class,
         ],
